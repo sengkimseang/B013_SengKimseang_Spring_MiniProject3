c24cf72e43f65fb1d4758a46757f2c9af8c31c3b
sengkimseang <tangdavidseang@gmail.com>
2017-06-21 10:23:41 +0700

    My First Commit

---
+++ src/main/java/org/krd/khmer/B013SengKimseangMiniprojectApplication.java
@@ -0,0 +1,12 @@
+package org.krd.khmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B013SengKimseangMiniprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B013SengKimseangMiniprojectApplication.class, args);
+	}
+}
